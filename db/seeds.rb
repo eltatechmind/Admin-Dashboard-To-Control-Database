d6f1f42b6c405adf655733d4e5b202f599120fd5
Elta3lab <ahmedfouad2020era@gmail.com>
2018-08-01 21:30:02 +0200

    create activeadmin dashboard completly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'sherifnegm@example.com', password: '12345678', password_confirmation: '12345678') if Rails.env.development?
+AdminUser.create!(email: 'sherifnegm@example.com', password: '12345678', password_confirmation: '12345678') if Rails.env.production?
